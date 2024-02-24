fdbfebff5dd5b3eb10d3a028cb052cde563a0a3b
HuijieLi <lihuijie0309@gmail.com>
2024-02-24 19:52:15 +0100

    update notebook and py file

--- wp2/codes/auto_annotation.py
+++ wp2/codes/auto_annotation.py
@@ -5,54 +5,24 @@ import markerrepo.parsing as pars
 import markerrepo.utils as utl
 import scanpy as sc
 import matplotlib.pyplot as plt
+from IPython.display import display
 import pandas as pd
 import os
 import inspect
 import re
 import math
+import warnings
 
-repo_path = "/mnt/workspace_stud/stud4/annotate_by_marker_and_features-sort"
-h5ad_path = "/mnt/workspace_stud/allstud/wp1/data/2024_01_28/heart_lv_SM-IOBHO.h5ad"
+try:
+    from sctoolbox.tools import celltype_annotation
+except ModuleNotFoundError:
+    warnings.warn("Please install the latest sctoolbox version. Some functionality may not be available.", RuntimeWarning)
 
-adata = sc.read_h5ad(h5ad_path)
-annot.list_possible_settings(repo_path, adata=adata)
 
-# Taxonomy ID or Organism Name
-# e.g., "human" or 9606
-organism = "human"
+def get_clustering_column_list(adata):
+    return list(adata.uns["clusters"].keys())
 
-# Column in .obs table where ranked genes groups are stored
-# e.g., "rank_genes_groups"
-# Enter None if no ranking has been performed yet
-rank_genes_column = None
-
-# Column in .var table where gene symbols or Ensembl IDs are stored
-# Enter None if the index column of the .var table already has gene symbols or Ensembl IDs
-# that you want to use for your annotation
-genes_column = None
-
-# The .obs table column of the clustering you want to annotate (e.g., "leiden" or "louvain")
-# If None, you can pick one interactively 
-clustering_column_lists = list(adata.uns["clusters"].keys())
-#clustering_column_lists = ["lovain_0.1", "lovain_0.25", "lovain_0.5", "lovain_1", "leiden_0.1", "leiden_0.25", "leiden_0.5", "leiden_1", "kmeans_13", "kmeans_16"]
-
-# Specify whether your index of the .var tables are Ensembl IDs (True) or gene symbols (False)
-ensembl = mr.check_ensembl(adata)
-
-# Name of the column to add with the final cell type annotation
-# If None, all annotation columns will be kept
-celltype_column_name = None
-
-# Whether to delete the created marker lists after annotation or not
-delete_lists = True
-
-# 
-column_specific_terms={"Source":"panglao", "Organism name":"human"}
-
-# 
-mr_parameters = [{"style":"two_column", "file_name":"two_column"}]
-
-def validate_settings(settings=None, repo_path=None, adata=None, organism=None, rank_genes_column=None, genes_column=None, 
+def auto_validate_settings(settings=None, repo_path=None, adata=None, organism=None, rank_genes_column=None, genes_column=None, 
                       clustering_column=None, ensembl=None, col_to_search=None, search_terms=None, column_specific_terms=None):
     """
     Validates user settings including file paths, anndata object columns, and specified organism.
@@ -171,10 +141,7 @@ def validate_settings(settings=None, repo_path=None, adata=None, organism=None, 
         formatted_var_columns = "\n  - " + "\n  - ".join(adata.var.columns)
         errors.append(f"Invalid genes_column {genes_column}.\nAvailable columns in adata.var:{formatted_var_columns}\n")
 
-#    if clustering_column and clustering_column not in adata.obs.columns:
-#        formatted_obs_columns = "\n  - " + "\n  - ".join(adata.obs.columns)
-#        errors.append(f"Invalid column {clustering_column}.\nAvailable columns in adata.obs:{formatted_obs_columns}\n")
-
+    # --- change --- #
     if isinstance(clustering_column, list):
         for column in clustering_column:
             if column and column not in adata.obs.columns:
@@ -239,15 +206,7 @@ def validate_settings(settings=None, repo_path=None, adata=None, organism=None, 
 
     return True
 
-validate_settings(settings=mr_parameters, repo_path=repo_path, adata=adata, organism=organism, 
-                        rank_genes_column=rank_genes_column, genes_column=genes_column, clustering_column=clustering_column_lists, ensembl=ensembl,
-                        column_specific_terms=column_specific_terms)
-
-marker_lists = wrap.create_multiple_marker_lists(settings=mr_parameters, repo_path=repo_path, organism=organism, 
-                                                 ensembl=ensembl, column_specific_terms=column_specific_terms,
-                                                 show_lists=True, adata=adata)
-
-def show_tables(annotation_dir=None, n=5, clustering_column="leiden_0.1", show_diff=True):
+def auto_show_tables(annotation_dir=None, n=5, clustering_column="leiden_0.1", show_diff=True):
     """
     Display dataframes for each cluster showing scores, hits, number of genes, mean of UI. Optionally, 
     it can also show both normal and scaled normalized differences of every potential cell type.
@@ -277,8 +236,6 @@ def show_tables(annotation_dir=None, n=5, clustering_column="leiden_0.1", show_d
         ct_column = f"Cluster {cluster}"
         df = pd.read_csv(f'{path}/{file}', sep='\t', names=[ct_column, "Score", "Hits", "Number of marker genes", "Mean of UI"])
 
-        #df = df.sort_values(by='Score', ascending=False).reset_index(drop=True)
-
         if show_diff:
             # Calculate and add both normal and scaled diffs to the DataFrame if show_diff is True
             normal_diffs, scaled_diffs = annot.calculate_normalized_diffs(df.rename(columns={ct_column: "Cell type"}))
@@ -286,14 +243,14 @@ def show_tables(annotation_dir=None, n=5, clustering_column="leiden_0.1", show_d
             df['Scaled Diff'] = df[ct_column].apply(lambda x: scaled_diffs.get(x, 0))
 
         cluster_dict[ct_column] = df.head(n)
-        display(df.head(n))
 
     return cluster_dict
 
-def run_annotation(adata, marker_repo=True, SCSA=True, marker_lists=None, mr_obs="mr", scsa_obs="scsa", 
-                   rank_genes_column=None, clustering_column=None, reference_obs=None, keep_all=False, 
-                   verbose=False, show_ct_tables=False, show_plots=False, show_comparison=False, ignore_overwrite=True,
+def auto_run_annotation(adata, marker_repo=True, SCSA=True, marker_lists=None, mr_obs="mr", scsa_obs="scsa", 
+                   rank_genes_column=None, clustering_column_list=None, reference_obs=None, keep_all=False, 
+                   verbose=False, show_ct_tables=False, show_plots=False, ignore_overwrite=True,
                    celltype_column_name=None):
+
     """
     Performs annotations on single cell data and allows the user to choose between different annotation methods. 
 
@@ -326,81 +283,73 @@ def run_annotation(adata, marker_repo=True, SCSA=True, marker_lists=None, mr_obs
         If True, the function will show the tables of the annotation.
     show_plots : bool, default False
         If True, the function will show the plots of the annotation.
-    show_comparison : bool, default False
-        If True, the function will show the comparison of the annotations.
     ignore_overwrite : bool, default False
         If True, the function will not ask for confirmation before overwriting existing files.
     celltype_column_name : str, default None
         The name of the selected cell type annotation column. If None, all annotation columns will be kept.
     """
-
-    if not marker_repo and not SCSA:
-        raise ValueError("At least one of 'marker_repo' or 'SCSA' must be True.")
-
-    if marker_lists is None or not marker_lists:
-        raise ValueError("No marker lists provided. Please provide a list of marker list paths.")
     
-    for marker_list in marker_lists:
-        if not os.path.exists(marker_list):
-            raise FileNotFoundError(f"Marker list file not found: {marker_list}")
+    # --- change --- #
+    annotation_results = []
+
+    # --- change --- #
+
+    for clustering_column in clustering_column_list:
+
+        if not marker_repo and not SCSA:
+            raise ValueError("At least one of 'marker_repo' or 'SCSA' must be True.")
+
+        if marker_lists is None or not marker_lists:
+            raise ValueError("No marker lists provided. Please provide a list of marker list paths.")
         
-    if not clustering_column:
-        clustering_column = mr.select(whitelist=list(adata.obs.columns), heading="clustering column")
-
-    if clustering_column not in adata.obs:
-        raise ValueError(f"Clustering column '{clustering_column}' not found in adata.obs.")
-
-    if rank_genes_column is not None and rank_genes_column not in adata.uns:
-        raise ValueError(f"Rank genes column '{rank_genes_column}' not found in adata.uns.")
-
-    if reference_obs is not None and reference_obs not in adata.obs:
-        raise ValueError(f"Reference annotation column '{reference_obs}' not found in adata.obs.")
-    
-    if not rank_genes_column:
-        rank_genes_column = wrap.rank_genes(adata, clustering_column, show_plots=show_plots, verbose=verbose)
-
-    annotation_columns = [] if reference_obs is None else [reference_obs]        
-
-    for marker_list in marker_lists:
-        name = marker_list.split('/')[-1]
-        annotation_dir = f"./annotation/{name}"
-        plot_columns = [] if reference_obs is None else [reference_obs]
-
-        if marker_repo:
-            ct_column = f"{mr_obs}_{name}"
-            annotation_columns.append(ct_column)
-            plot_columns.append(ct_column)
+        for marker_list in marker_lists:
+            if not os.path.exists(marker_list):
+                raise FileNotFoundError(f"Marker list file not found: {marker_list}")
             
-            # Execute Marker Repo annotation
-            annot.annot_ct(adata, output_path=annotation_dir, db_path=marker_list,
-                           cluster_column=clustering_column, rank_genes_column=rank_genes_column, 
-                           ct_column=ct_column, verbose=verbose, ignore_overwrite=ignore_overwrite)
+        if not clustering_column:
+            clustering_column = mr.select(whitelist=list(adata.obs.columns), heading="clustering column")
 
-            # Show tables and alternative cell types of each cluster
-            if show_ct_tables:
-                print(f"Tables of cell type annotation with clustering {clustering_column} and marker list {name}:")
-                #annot.show_tables(annotation_dir=annotation_dir, n=5, clustering_column=clustering_column, show_diff=True)
-                show_tables(annotation_dir=annotation_dir, n=5, clustering_column=clustering_column, show_diff=True)
+        if clustering_column not in adata.obs:
+            raise ValueError(f"Clustering column '{clustering_column}' not found in adata.obs.")
 
-        if SCSA:
-            column_added = f"{scsa_obs}_{name}"
-            annotation_columns.append(column_added)
-            plot_columns.append(column_added)
+        if rank_genes_column is not None and rank_genes_column not in adata.uns:
+            raise ValueError(f"Rank genes column '{rank_genes_column}' not found in adata.uns.")
 
-            # Execute SCSA annotation
-            if verbose:
-                celltype_annotation.run_scsa(adata, 
-                    gene_column=None, 
-                    key=rank_genes_column, 
-                    column_added=column_added,
-                    inplace=True, 
-                    species=None, 
-                    fc=1.5, 
-                    pvalue=0.05, 
-                    user_db=annot.reformat_marker_list(marker_list), 
-                    celltype_column="cell_name")
-            else:
-                with wrap.suppress_logging(logger_name='sctoolbox'):
+        if reference_obs is not None and reference_obs not in adata.obs:
+            raise ValueError(f"Reference annotation column '{reference_obs}' not found in adata.obs.")
+        
+        if not rank_genes_column:
+            rank_genes_column = wrap.rank_genes(adata, clustering_column, show_plots=show_plots, verbose=verbose)
+
+        annotation_columns = [] if reference_obs is None else [reference_obs]        
+
+        for marker_list in marker_lists:
+            name = marker_list.split('/')[-1]
+            annotation_dir = f"./annotation/{name}"
+            plot_columns = [] if reference_obs is None else [reference_obs]
+
+            if marker_repo:
+                ct_column = f"{mr_obs}_{name}"
+                annotation_columns.append(ct_column)
+                plot_columns.append(ct_column)
+                
+                # Execute Marker Repo annotation
+                annot.annot_ct(adata, output_path=annotation_dir, db_path=marker_list,
+                            cluster_column=clustering_column, rank_genes_column=rank_genes_column, 
+                            ct_column=ct_column, verbose=verbose, ignore_overwrite=ignore_overwrite)
+
+                # Show tables and alternative cell types of each cluster
+                if show_ct_tables:
+                    print(f"Tables of cell type annotation with clustering {clustering_column} and marker list {name}:")
+                    auto_show_tables(annotation_dir=annotation_dir, n=5, clustering_column=clustering_column, show_diff=True)
+
+            if SCSA:
+                column_added = f"{scsa_obs}_{name}"
+                annotation_columns.append(column_added)
+                plot_columns.append(column_added)
+
+                # Execute SCSA annotation
+                if verbose:
                     celltype_annotation.run_scsa(adata, 
                         gene_column=None, 
                         key=rank_genes_column, 
@@ -411,45 +360,45 @@ def run_annotation(adata, marker_repo=True, SCSA=True, marker_lists=None, mr_obs
                         pvalue=0.05, 
                         user_db=annot.reformat_marker_list(marker_list), 
                         celltype_column="cell_name")
+                else:
+                    with wrap.suppress_logging(logger_name='sctoolbox'):
+                        celltype_annotation.run_scsa(adata, 
+                            gene_column=None, 
+                            key=rank_genes_column, 
+                            column_added=column_added,
+                            inplace=True, 
+                            species=None, 
+                            fc=1.5, 
+                            pvalue=0.05, 
+                            user_db=annot.reformat_marker_list(marker_list), 
+                            celltype_column="cell_name")
 
-        umap_plot_file = f"{clustering_column}.png"
+            umap_plot_file = f"{clustering_column}.png"
 
-        # Show plots
-        if show_plots:
-            sc.pl.umap(adata, color=column, wspace=0.5, cmap=None, save=umap_plot_file)
-
-    # Compare annotations
-    if show_comparison:
-        print("Comparison of cell type annotations:")
-
+            # Show plots
+            if show_plots:
+                sc.pl.umap(adata, color=clustering_column, wspace=0.5, cmap=None, save=umap_plot_file)
         
-        display(annot.compare_cell_types(adata, clustering_column, annotation_columns))
-    
-    # Select cell type annotation
-    if celltype_column_name:
-        annotation_column = mr.select(whitelist=annotation_columns, heading="Select cell type annotation column:")
-        adata.obs.rename(columns={annotation_column: celltype_column_name}, inplace=True)
-    else:
-        keep_all = True
+        # Select cell type annotation
+        if celltype_column_name:
+            annotation_column = mr.select(whitelist=annotation_columns, heading="Select cell type annotation column:")
+            adata.obs.rename(columns={annotation_column: celltype_column_name}, inplace=True)
+        else:
+            keep_all = True
 
-    if not keep_all:
-        # Keep only the selected annotation column and reference_obs if provided
-        columns_to_keep = [annotation_column]
-        if reference_obs is not None and reference_obs in adata.obs.columns:
-            columns_to_keep.append(reference_obs)
+        if not keep_all:
+            # Keep only the selected annotation column and reference_obs if provided
+            columns_to_keep = [annotation_column]
+            if reference_obs is not None and reference_obs in adata.obs.columns:
+                columns_to_keep.append(reference_obs)
 
-        columns_to_remove = [col for col in annotation_columns if col not in columns_to_keep]
-        adata.obs.drop(columns=columns_to_remove, inplace=True)
+            columns_to_remove = [col for col in annotation_columns if col not in columns_to_keep]
+            adata.obs.drop(columns=columns_to_remove, inplace=True)
 
-    return clustering_column, annot.compare_cell_types(adata, clustering_column, annotation_columns), umap_plot_file, show_tables(annotation_dir=annotation_dir, n=5, clustering_column=clustering_column, show_diff=True)
+        annotation_results.append(clustering_column, annot.compare_cell_types(adata, clustering_column, annotation_columns), umap_plot_file, auto_show_tables(annotation_dir=annotation_dir, n=5, clustering_column=clustering_column, show_diff=True))
 
-annotation_df_list = []
+    return annotation_results
 
-for column in clustering_column_lists:
-    annotation_df_list.append(run_annotation(adata, SCSA=False, marker_lists=marker_lists, reference_obs=None, show_comparison=True,
-                    clustering_column=column, rank_genes_column=rank_genes_column, 
-                    ignore_overwrite=True, verbose=False, show_plots=True, show_ct_tables=True, 
-                    celltype_column_name=celltype_column_name))
     
 def show_umap_collection(annotation_df_list, clustering_column_lists):
     pattern = r'^([a-zA-Z]+)'
@@ -497,16 +446,15 @@ def show_umap_collection(annotation_df_list, clustering_column_lists):
             plt.tight_layout()
             plt.show()
 
-show_umap_collection(annotation_df_list, clustering_column_lists)
 
-def create_compare_df(annotation_df_list):
-    cell_types = list(set(value for df in annotation_df_list for value in df[1].iloc[:,0]))
+def create_compare_df(adata, annotation_results, clustering_column_list):
+    cell_types = list(set(value for df in annotation_results for value in df[1].iloc[:,0]))
     cell_types.append("ari_score")
 
-    merge_df = pd.DataFrame(columns=clustering_column_lists, index=cell_types)
+    merge_df = pd.DataFrame(columns=clustering_column_list, index=cell_types)
     merge_df = merge_df.fillna("")
 
-    for data in annotation_df_list:
+    for data in annotation_results:
         col_name = data[0]
         df = data[1]
         for index, value in df.iloc[:,0].iteritems():
@@ -516,30 +464,14 @@ def create_compare_df(annotation_df_list):
                 else:
                     merge_df.at[value, col_name] += f", {index}"
 
-    for column in clustering_column_lists:
+    for column in clustering_column_list:
         merge_df.at["ari_score", column] = round(adata.uns["clusters"][column]["score"]["ari"], 5)
 
     return merge_df
 
-merge_df = create_compare_df(annotation_df_list)
-merge_df
 
-def find_cluster(methode, cluster_num):
-    for df in annotation_df_list:
+def find_cluster(methode, cluster_num, annotation_results):
+    for df in annotation_results:
         if methode == str(df[0]):
             clusters = df[3]
             display(clusters[f"Cluster {cluster_num}"])
-
-find_cluster("lovain_1", 2)
-
-best_cluter = str(adata.uns["best_cluster"])
-best_cluter
-
-reference_best = wrap.run_annotation(adata, SCSA=False, marker_lists=marker_lists, reference_obs="ontology label", show_comparison=False,
-                    clustering_column=best_cluter, rank_genes_column=rank_genes_column, 
-                    ignore_overwrite=True, verbose=False, show_plots=True, show_ct_tables=True, 
-                    celltype_column_name=celltype_column_name)
-
-if delete_lists:
-    mr.delete_files(marker_lists)
-
